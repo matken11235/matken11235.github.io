56742e58e00aa4c1fb2f2856feecd5cc31de18c9
Ken Matsui <26405363+matken11235@users.noreply.github.com>
2018-06-06 18:42:42 +0900

    Improve hoge.js

---
+++ js/hoge.js
@@ -0,0 +1,21 @@
+// Can also be included with a regular script tag
+// import Typed from 'typed.min.js';
+
+var options = {
+  // strings to be typed
+  strings: [
+      "Hello",
+      "I'm","matken11235",
+      "What","do","you","want","to","know","?"
+    ],
+  // type speed in milliseconds
+  typeSpeed: 20,
+  // Fade out instead of backspace
+  fadeOut: true,
+  // css class for fade animation
+  fadeOutClass: 'typed-fade-out',
+  // Fade out delay in milliseconds
+  fadeOutDelay: 500,
+};
+
+var typed = new Typed(".word-1", options);
